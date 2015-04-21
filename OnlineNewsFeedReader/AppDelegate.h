c939208537dc6f21daa829ac50063213fa56c777
Jiwei <majiwei@MaJiweis-MacBook-Air.local>
2015-04-21 13:47:13 +1000

    Initial Commit

---
+++ OnlineNewsFeedReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OnlineNewsFeedReader
+//
+//  Created by Jiwei on 21/04/2015.
+//  Copyright (c) 2015 Jiwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OnlineNewsFeedReader/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  OnlineNewsFeedReader
+//
+//  Created by Jiwei on 21/04/2015.
+//  Copyright (c) 2015 Jiwei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
